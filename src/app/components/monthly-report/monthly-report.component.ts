80b26040c164acd57c0c283f0a64046b9f09dd31
vray797 <vray797@gmail.com>
2024-02-26 20:53:31 +0530

    changes

---
+++ src/app/components/monthly-report/monthly-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './monthly-report.component.html',
+  styleUrl: './monthly-report.component.css'
+})
+export class MonthlyReportComponent {
+
+}
